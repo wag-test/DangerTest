874688b20c3e45107b47a06d5a5ea17fad18e3f2
Miles Hollingsworth <miles.hollingsworth@wagwalking.com>
2018-07-09 11:16:37 -0700

    Initial Commit

---
+++ DangerTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DangerTest
+//
+//  Created by Miles Hollingsworth on 7/9/18.
+//  Copyright © 2018 Wag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
